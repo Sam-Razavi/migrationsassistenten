b1573f5abc3a2744eb64c823ea2423cec75cb7d6
Sam Sahbaei Razavi <h24sasah@du.se>
2026-03-06 15:45:00 +0000

    Wire API client to send Authorization header

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,8 +1,31 @@
 import axios from 'axios'
 
+const TOKEN_KEY = 'migration_jwt'
+
 const client = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
   headers: { 'Content-Type': 'application/json' },
 })
+
+client.interceptors.request.use(config => {
+  const token = localStorage.getItem(TOKEN_KEY)
+  if (token) {
+    config.headers = config.headers ?? {}
+    config.headers['Authorization'] = `Bearer ${token}`
+  }
+  return config
+})
+
+client.interceptors.response.use(
+  res => res,
+  err => {
+    if (err.response?.status === 401) {
+      localStorage.removeItem(TOKEN_KEY)
+      localStorage.removeItem('migration_user')
+      window.location.href = '/login'
+    }
+    return Promise.reject(err)
+  }
+)
 
 export default client
